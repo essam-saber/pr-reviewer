38d3254055b48b093250bd87a602f2ef0608e123
esso <esso@debian.esso>
2026-06-03 14:41:45 +0300

    feat: add hmac method to verify the webhook request

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ fastify.post("/webhook", async (request, reply) => {
 });
 
 try {
-  await fastify.listen({ port: 3000 });
+  await fastify.listen({ port: 3000, host: "0.0.0.0"});
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
